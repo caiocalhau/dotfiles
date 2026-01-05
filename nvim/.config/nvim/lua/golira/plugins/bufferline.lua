3c566904a0809aa0ca02df5dbca7ca549442104c
Caio-Calhau <caiocalhaum@gmail.com>
2026-01-05 10:26:55 -0300

    refactor: syntax

--- nvim/.config/nvim/lua/golira/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/golira/plugins/bufferline.lua
@@ -1,30 +1,4 @@
 return {
-	-- {
-	-- 	"akinsho/bufferline.nvim",
-	-- 	version = "*",
-	-- 	dependencies = "nvim-tree/nvim-web-devicons",
-	-- 	opts = {
-	-- 		options = {
-	-- 			mode = "tabs",
-	-- 		},
-	-- 	},
-	-- 	diagnostics = "nvim_lsp",
-	-- 	diagnostics_update_in_insert = true, -- only applies to coc
-	-- 	diagnostics_update_on_event = true, -- use nvim's diagnostic handler
-	--
-	-- 	config = function()
-	-- 		local bufferline = require("bufferline")
-	-- 		bufferline.setup({
-	-- 			options = {
-	-- 				style_preset = {
-	-- 					bufferline.style_preset.no_italic,
-	-- 					bufferline.style_preset.no_bold,
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
-
 	{
 		"tiagovla/scope.nvim",
 
